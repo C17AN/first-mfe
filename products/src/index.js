d2a97e0fb65fbbdb31e1ec3a06306df978710f34
c17an <kimchanmin1@gmail.com>
2021-08-12 21:08:21 +0900

    초기 프로젝트 구성

---
+++ products/src/index.js
@@ -0,0 +1,10 @@
+// 목업 데이터 생성용
+import faker from "faker";
+
+let products = "";
+for (let i = 0; i < 3; i++) {
+  const name = faker.commerce.productName();
+  products += `<div>${name}</div>`;
+}
+
+document.querySelector("#dev-products").innerHTML = products;
